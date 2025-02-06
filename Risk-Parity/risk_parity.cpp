e923d6922ec9fc1d2d809931d9266e663dfc39cf
Sachin <sachinadlakha1801@gmail.com>
2025-02-06 15:32:07 -0500

    Initial Commit

---
+++ Risk-Parity/risk_parity.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<fstream> //for file input/output 
+#include<sstream>  // for string parsing using sstream
+#include<vector> 
+#include<string>
+#include<cmath>
+#include<algorithm>
+
